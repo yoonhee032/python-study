88245fc9513a7c323c1b7617585d9da5f034c8b7
yoonhee Lee <105180536+yoonhee032@users.noreply.github.com>
2023-12-07 12:46:24 +0900

    데이터와 변수 실습4(체중과 키를 입력받아 BMI 계산하기)

---
+++ 12_07/Practice/46_1.py
@@ -0,0 +1,16 @@
+# 입력받은 체중과 키가 숫자이면 BMI를 계산하여 출력하기
+
+weight = input('체중 입력(g): ')
+height = input('신장 입력(cm): ')
+
+if weight.isdigit():
+    weight = int(weight) / 10
+if height.isdigit():
+    height = int(height) / 100
+
+
+bmi = weight / (height * height)
+
+print('체중 : {}kg' .format(weight))
+print('신장 : {}m' .format(height))
+print('BMI : %.2f' % bmi)
