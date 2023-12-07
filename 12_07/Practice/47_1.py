9327c81b468a90847731be1cabaf99690dc41206
yoonhee Lee <105180536+yoonhee032@users.noreply.github.com>
2023-12-07 13:11:34 +0900

    데이터와 변수 실습5(선택한 언어에 따라 다른 메뉴판을 출력해보자)

---
+++ 12_07/Practice/47_1.py
@@ -0,0 +1,9 @@
+#선택한 언어에 따라 다른 메뉴판을 출력해보자
+
+choiceLanguage = input('언어 선택(Choose your language): 1.한국어 \t 2.English')
+
+if choiceLanguage == '1':
+    print('1.샌드위치 \t 2.햄버거 \t 3.쥬스 \t 4.커피 \t5.아이스크림')
+if choiceLanguage == '2':
+    print('1.sandwich \t 2.Hamburger \t 3.Juice \t 4.Coffee \t5.Icecream')
+
