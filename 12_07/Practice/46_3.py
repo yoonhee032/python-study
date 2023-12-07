e0bdaca75b206ce9766476dcb61e7ebe08e375c6
yoonhee Lee <105180536+yoonhee032@users.noreply.github.com>
2023-12-07 12:47:36 +0900

    데이터와 변수 실습5(중간, 기고사 평균 및 총합 계산하기)

---
+++ 12_07/Practice/46_3.py
@@ -0,0 +1,17 @@
+#중간, 기말고사 점수를 입력ㅎ면 총점과 평균이 출력되는 프로그램 만들기
+
+middleScore = input('중간 고사 점수: ')
+finalScore = input('기말 고사 점수: ')
+
+if(middleScore.isdigit() and finalScore.isdigit()):
+    middleScore = int(middleScore)
+    finalScore = int(finalScore)
+
+    totalScore = middleScore + finalScore
+    avg = (middleScore + finalScore) / 2
+
+    print('총점 : {} , 평균 : {}' .format(totalScore, avg))
+
+else:
+    print('잘못 입력했습니다.')
+
