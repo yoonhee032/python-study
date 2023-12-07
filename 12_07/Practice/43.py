73a3b707bda3810fa70d6b190a4588dd224a8348
yoonhee Lee <105180536+yoonhee032@users.noreply.github.com>
2023-12-07 11:30:56 +0900

    데이터와 변수 실습1

---
+++ 12_07/Practice/43.py
@@ -0,0 +1,31 @@
+name = '홍길동'
+product = '야구글러브'
+orderNo = 2568956
+payMethod = '신용카드'
+productPrice = 110000
+payPrice = 100000
+point = 10000
+payDate = '2021/08/03 23:50:12'
+payDiv = 6
+payDivCategory = '무'
+contactNum = '02-1234-5678'
+
+
+print(name, ' 고객님 안녕하세요.')
+print(name, ' 고객님의 주문이 완료되었습니다.')
+print('다음은 주문건에 대한 상세 내역입니다.')
+print('-' * 50)
+print('상품명\t: ', product)
+print('주문번호\t: ', orderNo)
+print('결제방법\t: ', payMethod)
+print('상품금액\t: ', productPrice)
+print('결제금액\t: ', payPrice)
+print('포인트\t: ', point)
+print('결제일시\t: ', payDate)
+print('할부\t\t: ', payDiv)
+print('할부유형\t: ', payDivCategory)
+print('문의\t\t: ', contactNum)
+
+
+
+
